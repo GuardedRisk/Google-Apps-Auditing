dde69b33fd2d6b571d7211ba34c20204be1f6d30
Bert JW Regeer <bertjw@regeer.org>
2014-01-03 23:29:13 -0700

    Import default project structure

---
+++ setup.py
@@ -0,0 +1,49 @@
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README')).read()
+CHANGES = open(os.path.join(here, 'CHANGES')).read()
+
+requires = [
+        'oauth2client',
+    ]
+
+tests_require = []
+
+testing_requires =  tests_require + [
+    'nose',
+    'coverage',
+    ]
+
+develop_requires = [
+    ]
+
+setup(name='acctwatch',
+      version='0.0',
+      description='acctwatch',
+      long_description=README + '\n\n' +  CHANGES,
+      classifiers=[
+        "Programming Language :: Python",
+        ],
+      author='Bert JW Regeer',
+      author_email='bertjw@regeer.org',
+      url='',
+      keywords='',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      test_suite='acctwatch.tests',
+      install_requires=requires,
+      tests_require=tests_require,
+      extras_require = {
+          'develop': develop_requires,
+          'testing': testing_requires,
+          },
+      entry_points="""\
+      [console_scripts]
+      acctwatch = acctwatch.acctwatch:main
+      """,
+      )
+
