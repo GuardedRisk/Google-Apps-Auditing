12fdeb95224741d466b4876cb063a23270260ed6
Bert JW Regeer <bertjw@regeer.org>
2014-01-04 22:30:36 -0700

    Use new Configuration class

--- acctwatch/acctwatch.py
+++ acctwatch/acctwatch.py
@@ -1,69 +1,26 @@
-import argparse
 import httplib2
 import os
 import sys
-from ConfigParser import (
-        SafeConfigParser,
-        NoOptionError,
-        )
 
 from apiclient import discovery
 from oauth2client import file
 from oauth2client import client
 from oauth2client import tools
 
-# Parser for command-line arguments.
-parser = argparse.ArgumentParser(
-        description=__doc__,
-        formatter_class=argparse.RawDescriptionHelpFormatter,
-        parents=[tools.argparser]
-        )
-parser.add_argument('--section', nargs='?', help='The section to read from the configuration file.', default='DEFAULT')
-parser.add_argument('config', help='Configuration file.')
+from config import Configuration
+
+try:
+    import geoip2.database as geoipdb
+except ImportError:
+    geoipdb = None
 
 def main():
-    # Parse the command-line flags.
-    flags = parser.parse_args()
+    config = Configuration()
+    if config.WITH_GEOIP and not geoipdb:
+        print ("GeoIP is enabled, but unable to import module, please check installation. Disabling.")
+        config.WITH_GEOIP = False
 
-    config_defaults = {}
-    config_defaults['here'] = os.path.dirname(os.path.abspath(flags.config))
-    config = SafeConfigParser(config_defaults)
-    config.readfp(open(flags.config))
-
-    if flags.section != 'DEFAULT' and not config.has_section(flags.section):
-        parser.error('Specified section doesn\'t exist in configuration.')
-
-    try:
-        # CLIENT_SECRETS is name of a file containing the OAuth 2.0 information for this
-        # application, including client_id and client_secret. You can see the Client ID
-        # and Client secret on the APIs page in the Cloud Console:
-        # <https://cloud.google.com/console>
-        CLIENT_SECRETS = config.get(flags.section, 'secrets')
-        CLIENT_CREDENTIALS = config.get(flags.section, 'credentials')
-        INTERVAL = int(config.get(flags.section, 'interval'))
-    except NoOptionError as e:
-        parser.error('Unable to get required parameter from config: {}'.format(e))
-
-    # Set up a Flow object to be used for authentication.
-    # Add one or more of the following scopes. PLEASE ONLY ADD THE SCOPES YOU
-    # NEED. For more information on using scopes please see
-    # <https://developers.google.com/+/best-practices>.
-    FLOW = client.flow_from_clientsecrets(CLIENT_SECRETS,
-            scope=[
-                'https://www.googleapis.com/auth/admin.reports.audit.readonly',
-                'https://www.googleapis.com/auth/admin.reports.usage.readonly',
-                ],
-            message=tools.message_if_missing(CLIENT_SECRETS)
-            )
-
-    # If the credentials don't exist or are invalid run through the native client
-    # flow. The Storage object will ensure that if successful the good
-    # credentials will get written back to the file.
-    storage = file.Storage(CLIENT_CREDENTIALS)
-    credentials = storage.get()
-    if credentials is None or credentials.invalid:
-        credentials = tools.run_flow(FLOW, storage, flags)
-    print "Token expires at: " + str(credentials.token_expiry)
+    credentials = config.get_credentials()
 
     # Create an httplib2.Http object to handle our HTTP requests and authorize it
     # with our good Credentials.
