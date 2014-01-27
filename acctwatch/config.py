80c2be9ce5901b2198d2dc4c3231d34c1ffce5b5
Bert JW Regeer <bertjw@regeer.org>
2014-01-27 16:16:44 -0700

    Update the Configuration object

--- acctwatch/config.py
+++ acctwatch/config.py
@@ -26,6 +26,7 @@ class Configuration(object):
     def __init__(self):
         # Parse the command-line flags.
         flags = parser.parse_args()
+        self.flags = flags
 
         config_defaults = {}
         config_defaults['here'] = os.path.dirname(os.path.abspath(flags.config))
@@ -33,7 +34,7 @@ class Configuration(object):
         config.readfp(open(flags.config))
 
         if flags.section != 'DEFAULT' and not config.has_section(flags.section):
-            parser.error('Specified section doesn\'t exist in configuration.')
+            parser.error("Specified section doesn't exist in configuration.")
 
         try:
             # CLIENT_SECRETS is name of a file containing the OAuth 2.0 information for this
@@ -43,10 +44,15 @@ class Configuration(object):
             self.CLIENT_SECRETS = config.get(flags.section, 'secrets')
             self.CLIENT_CREDENTIALS = config.get(flags.section, 'credentials')
             self.INTERVAL = int(config.get(flags.section, 'interval'))
-            self.WITH_GEOIP = str2bool(config.get(flags.section, 'geoip.enabled'))
+            self.GEOIPDB = config.get(flags.section, 'geoip_db')
             self.email = {}
         except NoOptionError as e:
             parser.error('Unable to get required parameter from config: {}'.format(e))
+
+        # Get the rest of the parameters into a settings dictionary that can be
+        # retrieved from this object
+
+        self.settings = dict(config.items(flags.section))
 
         # Set up a Flow object to be used for authentication.
         # Add one or more of the following scopes. PLEASE ONLY ADD THE SCOPES YOU
@@ -63,10 +69,11 @@ class Configuration(object):
         # If the credentials don't exist or are invalid run through the native client
         # flow. The Storage object will ensure that if successful the good
         # credentials will get written back to the file.
-        storage = file.Storage(self.CLIENT_CREDENTIALS)
-        self.credentials = storage.get()
-        if self.credentials is None or self.credentials.invalid:
-            self.credentials = tools.run_flow(FLOW, storage, flags) 
+        self.storage = file.Storage(self.CLIENT_CREDENTIALS)
+        self.credentials = self.storage.get()
 
     def get_credentials(self):
+        if self.credentials is None or self.credentials.invalid:
+            self.credentials = tools.run_flow(self.FLOW, self.storage, self.flags)
+
         return self.credentials
