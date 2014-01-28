6fafb0478031117d523bae42bb8e62a3e257bc81
Bert JW Regeer <bertjw@regeer.org>
2014-01-27 17:17:38 -0700

    Add check to acctwatch_configcheck for database

--- acctwatch/configcheck.py
+++ acctwatch/configcheck.py
@@ -8,7 +8,10 @@ from oauth2client import file
 from oauth2client import client
 from oauth2client import tools
 
+from sqlalchemy.exc import OperationalError
+
 from config import Configuration
+from models import *
 
 try:
     import geoip2.database as geoipdb
@@ -28,11 +31,21 @@ def main():
     service = discovery.build('admin', 'reports_v1', http=http)
     activities = service.activities()
 
+    settings = config.settings
+    engine = engine_from_config(settings, 'sqlalchemy.')
+    DBSession.configure(bind=engine)
+
     try:
         login_list = activities.list(userKey='all', applicationName='login', maxResults=1000).execute()
+        DBSession.query(LoginItem).first()
+
         print("Success!")
     except client.AccessTokenRefreshError:
         print("Failure. Access token is invalid. Please re-run the tool to get a new access token.")
+    except OperationalError:
+        print("Database has not been initialised. Please run acctwatch_initdb.")
+    except:
+        print("Some other unknown error occured")
 
 if __name__ == '__main__':
     main()
